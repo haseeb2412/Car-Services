a5a585d75de6dc1c3eb605fa74ae67370102faaf
haseeb2412 <haseeb241202@gmail.com>
2024-05-23 22:58:28 +0500

    add some comments

--- src/assets/data/carData.js
+++ src/assets/data/carData.js
@@ -151,4 +151,7 @@ const carData = [
 
 export default carData;
 
-
+//   automatic: "Automatic",
+  //   description:
+  //     " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  // },
